112dde0f4f5d04fabb1a5378e18e02bda61a6285
kosher <lionel.kossi@gmail.com>
2022-04-10 21:00:34 +0000

    Add background task

--- background.js
+++ background.js
@@ -3,4 +3,5 @@ let color = '#3aa757'
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({color})
     console.log('Default background color set to %cgreen', `color: ${color}`)
+    console.log(navigator.clipboard.readText())
 })
